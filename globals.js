84e296acad76da91d9a898ec199945ab43d587e1
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2018-02-05 17:19:19 +0100

    First commit

---
+++ globals.js
@@ -0,0 +1,34 @@
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"165090BD-74DF-41A1-BD16-6131EF788FA7",variableType:12}
+ */
+var ltg_elementname = null;
+
+/**
+ * @type {Number}
+ *
+ * @properties={typeid:35,uuid:"7C04CAF9-AD2C-4210-837F-88453F04DD22",variableType:4}
+ */
+var ltg_print_list_id = null;
+
+/**
+ * @type {Number}
+ *
+ * @properties={typeid:35,uuid:"65BFFDEF-0765-443D-BB6B-FAD49E6C708F",variableType:4}
+ */
+var ltg_program_field_id = null;
+
+/**
+ * @type {Number}
+ *
+ * @properties={typeid:35,uuid:"F0FCDF39-D769-4732-85F0-9B205FB21795",variableType:4}
+ */
+var ltg_program_field_id$sort = null;
+
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"A743C4BE-2D8E-479A-8FB3-118DCF8B6537",variableType:12}
+ */
+var ltg_program_name = null;
